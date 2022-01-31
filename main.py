8e09614b3eee2cb309e414bc823cca3f0dbbcbb0
beyond <bianglibeyond@gmail.com>
2022-01-31 15:27:49 +0800

    add jetson codes

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-print("Hello jetson!\r\n")
+import RPi.GPIO as GPIO
+import time
 
-print("Hello from the other side!")
+# Pin Definitons:
+motorPin12 = 12  # BOARD pin 12
+
+def main():
+    prev_value = None
+
+    # Pin Setup:
+    GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
+    GPIO.setup(motorPin12, GPIO.OUT)
+
+    curr_sig = GPIO.LOW
+    oppo_sig = GPIO.HIGH
+    GPIO.output(motorPin12, curr_sig)
+    print("Starting demo now! Press CTRL+C to exit")
+    try:
+        while True:
+            GPIO.output(motorPin12, oppo_sig)
+            temp = oppo_sig
+            oppo_sig = curr_sig
+            curr_sig = temp
+            print("Outputting {} to Pin {}".format(curr_sig, motorPin12))
+            time.sleep(1)
+    finally:
+        GPIO.cleanup()  # cleanup all GPIO
+
+if __name__ == '__main__':
+    main()
