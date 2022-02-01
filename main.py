70152077f791941b06fea56423c3bc1794b9e339
beyond <bianglibeyond@gmail.com>
2022-02-01 16:18:42 +0800

    4 motors

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     # Pin Setup:
     GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
 
-    motorPins = [7, 11, 15, 19]
+    motorPins = [7, 11, 18, 19]
 
     control = ControlThread(motorPins=motorPins)
     control.start()
