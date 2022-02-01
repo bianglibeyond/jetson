5bdf8471d967f27353b9a285706fd1dae49b6498
beyond <bianglibeyond@gmail.com>
2022-02-01 13:31:59 +0800

    add thread class

--- main.py
+++ main.py
@@ -1,53 +1,56 @@
+from concurrent.futures import thread
+from msvcrt import setmode
 import RPi.GPIO as GPIO
-import time
+import time, threading
 from datetime import datetime
 
-# Pin Definitons:
-motorPin12 = 12  # BOARD pin 12
+
 
 def main():
-    pwmStrength = 2
+    pwmStrength = 0
 
     # Pin Setup:
     GPIO.setmode(GPIO.BOARD)  # BOARD pin-numbering scheme
-    GPIO.setup(motorPin12, GPIO.OUT)
-    GPIO.output(motorPin12, GPIO.LOW)
-    print("Starting demo now! Press CTRL+C to exit")
+
+    motor1 = MotorThread(name="Motor1", jetsonPin=12, pwmStrength=pwmStrength)
+    motor1.run()
+
     try:
-        isPWM = True
         while True:
-            
-            if isPWM:
-                timeStartS = datetime.now()
-                # 1s loop
-                while(True):
-                    n = 0
-                    while n<10:
-                        if n<pwmStrength:
-                            curr_sig = GPIO.HIGH
-                            GPIO.output(motorPin12, curr_sig)
-                        else:
-                            curr_sig = GPIO.LOW
-                            GPIO.output(motorPin12, curr_sig)
-                        n += 1
-                    timePeriod = (datetime.now()-timeStartS).total_seconds()
-                    if timePeriod>1.0:
-                        break
-                isPWM = not isPWM
-                print("Outputting {} to Pin {}".format(pwmStrength, motorPin12))
-            else:
-                GPIO.output(motorPin12, GPIO.HIGH)
-                isPWM = not isPWM
-                print("Outputting {} to Pin {}".format(10, motorPin12))
-                time.sleep(1)
-            
-            print("\r\nTime is {}\r\n".format(datetime.now()))
+            pwmStrength = int(input("Set PWM capacity(0-10): "))
+            motor1.join()
+            while motor1.is_alive():
+                pass
+            motor1 = MotorThread(name="Motor1", jetsonPin=12, pwmStrength=pwmStrength)
+            motor1.run()
+            print("Outputting {} to Pin {}".format(pwmStrength, 12))
     finally:
         GPIO.cleanup()  # cleanup all GPIO
+    
 
 
-
-# def setMotorPinPWM(pin, )
+class MotorThread(threading.Thread):
+    def __init__(self, name: int, jetsonPin: int, pwmStrength: int):
+        self._stopevent = threading.Event()
+        self._sleepperiod = 1.0
+        threading.Thread.__init__(self, name=name)
+        self.jetsonPin = jetsonPin
+        self.pwmStrength = pwmStrength
+    def run(self):
+        GPIO.setup(self.jetsonPin, GPIO.OUT)
+        GPIO.output(self.jetsonPin, GPIO.LOW)
+        print("Motor at Pin{} starts.".format(self.jetsonPin))
+        while not self._stopevent.is_set():
+            n = 0
+            while n<10:
+                if n<self.pwmStrength:
+                    GPIO.output(self.jetsonPin, GPIO.HIGH)
+                else:
+                    GPIO.output(self.jetsonPin, GPIO.LOW)
+                n += 1
+    def join(self, timeout=None):
+        self._stopevent.set()
+        threading.Thread.join(self, timeout)
 
 if __name__ == '__main__':
     main()
