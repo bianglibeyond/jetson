3afe07d96c1baa39464a58ade06258161bddb0bf
beyond <bianglibeyond@gmail.com>
2022-02-02 16:20:03 +0800

    global variable, add duration

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ motorStatus = {
             } 
         for n in (range(len(pins)))
     }
-# frequency = 200
-# the cycle time is roughly around 1.5-2.5 ms, 
-# frequency of 200 can make cycle time 5 ms, 
-# frequency of 300 can make cycle time 3.3 ms
+frequency = 200
+the cycle time is roughly around 1.5-2.5 ms, 
+frequency of 200 can make cycle time 5 ms, 
+frequency of 300 can make cycle time 3.3 ms
 
 
 
@@ -98,13 +98,13 @@ class MotorThread(threading.Thread):
                 else: 
                     GPIO.output(self.pin, GPIO.LOW)
                 n += 1
-            # timePassed = time.time() - startTime
-            # if timePassed<self.duration:
-            #     time.sleep(self.duration-timePassed)
-            #     # timePassed = time.time() - startTime
-            #     # print("\r\n{} at Pin{} has 1 cycle of {}ms".format(self.motorName, self.pin, timePassed*1000))
-            # else:
-            #     print("\r\n{} at Pin{} overrun DURATION of {}ms!".format(self.motorName, self.pin, self.duration*1000))
+            timePassed = time.time() - startTime
+            if timePassed<self.duration:
+                time.sleep(self.duration-timePassed)
+                # timePassed = time.time() - startTime
+                # print("\r\n{} at Pin{} has 1 cycle of {}ms".format(self.motorName, self.pin, timePassed*1000))
+            else:
+                print("\r\n{} at Pin{} overrun DURATION of {}ms!".format(self.motorName, self.pin, self.duration*1000))
     def join(self, timeout=None):
         self._stopevent.set()
         threading.Thread.join(self, timeout)
